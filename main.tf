7f556d298f02071a016ffdd85642d561b8fff1e4
Martin Chavez <mdchavez@dairy-farm.com.ph>
2026-07-13 13:46:00 +0800

    Fix SQL deployment connection

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "deploy_sql" {
 
   provisioner "local-exec" {
-    command = "sqlcmd -S host.docker.internal,1433 -U spacelift_dba -P ${var.sql_password} -C -i deploy.sql"
+    command = "sqlcmd -S pod10-mardc-lt,1433 -U spacelift_dba -C -i deploy.sql"
   }
 }
