31f509e1638fdd92ae2a0d4ac84290ea89ea5bda
Martin Chavez <mdchavez@dairy-farm.com.ph>
2026-07-14 10:10:44 +0800

    Add SQL connection variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "sql_server" {
+  type = string
+}
+
+variable "sql_database" {
+  type = string
+}
+
+variable "sql_user" {
+  type = string
+}
+
 variable "sql_password" {
   type      = string
   sensitive = true
