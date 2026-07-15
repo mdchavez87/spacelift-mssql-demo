16a3be5ecc194a6900d3b759271592e55effbe02
Martin Chavez <mdchavez@dairy-farm.com.ph>
2026-07-15 10:10:53 +0800

    Add Terraform outputs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "sql_server" {
+  description = "SQL Server address used for deployment"
+  value       = var.sql_server
+}
+
+output "sql_database" {
+  description = "SQL Database name used for deployment"
+  value       = var.sql_database
+}
