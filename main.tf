340f67b60e5ddb4cd67de39dd0e8ffb41dd630bf
Martin Chavez <mdchavez@dairy-farm.com.ph>
2026-07-15 13:03:12 +0800

    Improve SQL migration execution eror handling

--- main.tf
+++ main.tf
@@ -10,8 +10,9 @@ resource "null_resource" "database_migration" {
 /opt/mssql-tools18/bin/sqlcmd \
 -S ${var.sql_server} \
 -U ${var.sql_user} \
--P ${var.sql_password} \
+-P "${var.sql_password}" \
 -C \
+-b \
 -d ${var.sql_database} \
 -i database/migrations/001_create_customer.sql
 EOT
