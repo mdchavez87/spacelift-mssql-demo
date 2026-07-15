cbf4faa559463376489b4131bbd3b6c68781b967
Martin Chavez <mdchavez@dairy-farm.com.ph>
2026-07-15 10:21:28 +0800

    Remove duplicate Terraform outputs

--- main.tf
+++ main.tf
@@ -9,12 +9,4 @@ resource "null_resource" "deploy_sql" {
   }
 }
 
-output "sql_server" {
-  description = "SQL Server address used for deployment"
-  value       = var.sql_server
-}
 
-output "sql_database" {
-  description = "SQL Database name used for deployment"
-  value       = var.sql_database
-}
